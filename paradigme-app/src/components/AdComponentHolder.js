e297801358e92f572aeb583cea8e1a23bb2c33d8
Mykyta Trufanov <trufanov.n@outlook.com>
2021-01-30 15:11:51 +0200

    Added functionality to show ad left comments

--- paradigme-app/src/components/AdComponentHolder.js
+++ paradigme-app/src/components/AdComponentHolder.js
@@ -1,26 +1,27 @@
 import React, {Component} from "react";
-import ImageArticleComponent from "./article/ImageArticleComponent";
-import YoutubeVideoComponent from "./article/YoutubeVideoComponent";
-import TextArticleComponent from "./article/TextArticleComponent";
+import ImageArticleComponent from "./ad/ImageArticleComponent";
+import YoutubeVideoComponent from "./ad/YoutubeVideoComponent";
+import TextArticleComponent from "./ad/TextArticleComponent";
 
 export default class AdComponentHolder extends Component {
 
     constructor(props) {
         super(props);
 
-        this.ad = props.ad;
+        this.elements = props.elements;
+        this.userId = props.userId;
     }
 
 
     render() {
         return (
-            this.ad.elements.map(el => {
+            this.elements.map(el => {
                 if (el.type === 'text') {
                     return <TextArticleComponent text={el.text}/>
                 }
 
                 if (el.type === 'image') {
-                    return <ImageArticleComponent imageLink={`http://localhost:8080/media/images/${this.ad.userId + '/' + el.src}.jpg`}/>
+                    return <ImageArticleComponent imageLink={`http://localhost:8080/media/images/${this.userId + '/' + el.src}.jpg`}/>
                 }
 
                 if (el.type === 'video') {
--- paradigme-app/src/pages/AdPage.js
+++ paradigme-app/src/pages/AdPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import {withRouter} from "react-router";
 import AdComponentHolder from "../components/AdComponentHolder";
+import AdReviewSection from "../components/AdReviewSection";
+import LeaveReviewComponent from "../components/LeaveReviewComponent";
 
 class AdPage extends Component {
 
@@ -36,40 +38,16 @@ class AdPage extends Component {
                     <i data-ad-type={ad.type}/>
                     <b>{ad.phoneNumber}</b>
                     <hr/>
-                    <AdComponentHolder ad={ad}/>
-                    {/*<div th:each="el: ${ad.elements}">*/}
-                    {/*    [(${el.markup()})]*/}
-                    {/*</div>*/}
-                    {/*<h3>Отзывы:</h3>*/}
-                    {/*<div className="ad-review" th:each="review: ${ad.reviews}">*/}
-                    {/*    <hr>*/}
-                    {/*        <p th:text="${review.userFullName}"></p>*/}
-                    {/*        <p>Оценка: <b>[(${review.mark})]</b></p>*/}
-                    {/*        <p th:if="${review.text != null && review.text.length != 0}"*/}
-                    {/*           th:text="${'Комментарий: ' + review.text}"></p>*/}
-                    {/*</div>*/}
-                    {/*<hr>*/}
-                    {/*    <form th:action="@{/ad/{adUrl}/review(adUrl=${ad.url})}" method="post" th:if="${user != null}">*/}
-                    {/*        <label>*/}
-                    {/*            Оценка:*/}
-                    {/*            <input type="number" min="1" max="5" value="5" name="mark">*/}
-                    {/*        </label>*/}
-                    {/*        <label>*/}
-                    {/*            Отзыв:*/}
-                    {/*            <textarea name="text" placeholder="Необязательно"></textarea>*/}
-                    {/*        </label>*/}
-                    {/*        <input name="username" type="hidden" th:value="${user.getId()}">*/}
-                    {/*            <input name="userFullName" type="hidden"*/}
-                    {/*                   th:value="${user.getFirstName() + ' ' + user.getLastName()}">*/}
-                    {/*                <input type="submit" value="Отправить">*/}
-                    {/*    </form>*/}
+                    <AdComponentHolder elements={ad.elements} userId={ad.userId}/>
+                    <AdReviewSection reviews={ad.reviews}/>
+                    <hr/>
+                    <LeaveReviewComponent ad={ad}/>
                 </>
             ) : (
                 <p>
                     Loading...
                 </p>
             )
-
         );
     }
 
